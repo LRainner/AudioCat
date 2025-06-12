d07bfca4d0b6e1a2ae50a7bc6f33515906fa52b2
LRainner <307759701@qq.com>
2025-06-13 00:25:39 +0800

    feat: use tary icon

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,38 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{
+    Manager,
+    menu::{Menu, MenuItem},
+    tray::TrayIconBuilder,
+};
+
 fn main() {
-    tauri_demo_lib::run()
+    tauri::Builder::default()
+        .setup(|app| {
+            // 创建菜单项
+            let quit_item = MenuItem::with_id(app, "quit", "退出", true, None::<&str>)?;
+            let show_item = MenuItem::with_id(app, "show", "显示窗口", true, None::<&str>)?;
+            let menu = Menu::with_items(app, &[&show_item, &quit_item])?;
+
+            // 构建托盘
+            TrayIconBuilder::new()
+                .icon(app.default_window_icon().unwrap().clone())
+                .menu(&menu)
+                .on_menu_event(|app, event| match event.id.0.as_str() {
+                    "quit" => {
+                        app.exit(0);
+                    }
+                    "show" => {
+                        let window = app.get_webview_window("main").unwrap();
+                        window.show().unwrap();
+                        window.set_focus().unwrap();
+                    }
+                    _ => {}
+                })
+                .build(app)?;
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri app");
 }
